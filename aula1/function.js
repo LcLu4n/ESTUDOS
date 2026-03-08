7fc4950197914177dad3bbc2fe69c784a3cde36e
LcLu4n <luanmaia6662@gmail.com>
2026-03-08 14:58:53 -0300

    fuction cart 08/03/26

--- aula1/function.js
+++ aula1/function.js
@@ -5,3 +5,20 @@
 
 myName ("lolo")*/
 
+/*const cart = [10.00, 36.99, 215.00, 45.00, 115.99, 94.99, 1.00]
+let finalPrice = 0
+
+function calculatePrice(price, discount) {
+    const result = (price + discount) / 100
+    return result
+}
+
+
+cart.forEach(value => {
+    if (finalPrice => 50.00){
+       const discount = calculatePrice(value,10)
+        finalPrice = finalPrice + (value-discount)
+    }
+});
+
+console.log(finalPrice)*/
